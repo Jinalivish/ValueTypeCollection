e0b4ed78b1a2ec01c1520e24e2656667d440c1f2
Jinalivish <jinali.vish@gmail.com>
2024-04-23 21:10:43 -0400

    Add project files.

---
+++ ValueTypeCollectionDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+namespace ValueTypeCollectionDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeCollection<int> intCollection = new TypeCollection<int>();
+            intCollection.AddItem(88);
+            intCollection.AddItem(66);
+            intCollection.AddItem(7);
+
+            Console.WriteLine("Items in the collection:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(intCollection.GetItem(i));
+            }
+
+            Console.WriteLine("\nSorted collection in descending order:");
+            var sortedList = intCollection.GetSortedDescending();
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
---
+++ ValueTypeCollectionDemo/TypeCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValueTypeCollectionDemo
+{
+    public class TypeCollection<T> where T : struct
+    {
+        private List<T> items;
+
+        public TypeCollection()
+        {
+            items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+        }
+
+        public T GetItem(int index)
+        {
+            if (index >= 0 && index < items.Count)
+            {
+                return items[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
+        }
+
+        public List<T> GetSortedDescending()
+        {
+            List<T> sortedList = new List<T>(items);
+            sortedList.Sort();
+            sortedList.Reverse();
+            return sortedList;
+        }
+    }
+}
